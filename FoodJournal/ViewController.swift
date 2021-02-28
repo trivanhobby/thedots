76e3942cadf3cd628b4bb7df9837845c80d958b7
Alexey <alexey@hush.cool>
2021-02-28 11:26:06 +0300

    Initial Commit

---
+++ FoodJournal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodJournal
+//
+//  Created by Ivan Trifonov on 2/28/21.
+//  Copyright © 2021 Ivan Trifonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
